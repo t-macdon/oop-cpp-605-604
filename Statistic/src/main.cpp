1d1a7cc2edaa27ae0968af0d10fedfcdc44d51dd
Michael Schumacher <schuma.mike@gmail.com>
2021-09-11 13:08:33 -0400

    Add unit test framework (doctest)
    Added tasks to handle build, clean, and test shortcuts.
    Use default VSCode shortcut of CTRL+SHIFT+B to pop open
    the list of available shortcuts.

--- Statistic/src/main.cpp
+++ Statistic/src/main.cpp
@@ -5,12 +5,13 @@
  *      Author: Michael Schumacher && Tyler MacDonald
  */
 
-#include <iostream>
-
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
 #include "Statistic.h"
 
 using namespace std;
 
+#if 0
 int main()
 {
     cout << "Starting!" << endl;
@@ -28,4 +29,30 @@ int main()
     cout << "Standard Deviation: " << statistic.getSTD() << endl;
 
     cout << "Ending!" << endl;
+}
+#endif
+
+TEST_CASE("Same number repeatedly") {
+    Statistic stat;
+    static const double onlyNumber = 10;
+
+    CHECK(stat.getAverage() == 0);
+    // TODO: Average of an empty set is 0... Is that what we want?
+    // Or should either/both of these tests start as NaN?
+    CHECK(stat.getSTD() == 0);
+
+    stat.add(onlyNumber);
+
+    CHECK(stat.getAverage() == onlyNumber);
+    // Here we say a single number's std is NaN... See above comment.
+    CHECK(std::isnan(stat.getSTD()));
+
+    // Continuosly add the same number to this list
+    for(int i=0; i<10; i++) {
+        stat.add(onlyNumber);
+        CHECK(stat.getAverage() == onlyNumber);
+        // The standard deviation of a set of 1 number should be 0.
+        // They don't deviate.
+        CHECK(stat.getSTD() == 0);
+    }
 }
