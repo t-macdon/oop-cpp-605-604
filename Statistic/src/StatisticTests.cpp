210c68cd2502773ed4f76154dfbaae795ff0308d
Michael Schumacher <schuma.mike@gmail.com>
2021-09-11 22:24:00 -0400

    Minor test refactor, added discussion test cases

--- Statistic/src/StatisticTests.cpp
+++ Statistic/src/StatisticTests.cpp
@@ -27,6 +27,19 @@ double calculateStandardDeviation(const std::vector<double>& input) {
     return sqrt(accumulator/(input.size()-1));
 }
 
+void verifyStatisticsMatch(const std::vector<double>& numbers) {
+    double mean = calculateMean(numbers);
+    double standardDeviation = calculateStandardDeviation(numbers);
+
+    Statistic stat;
+    for(double i : numbers) {
+        stat.add(i);
+    }
+    
+    CHECK(stat.getAverage() == doctest::Approx(mean));
+    CHECK(stat.getSTD() == doctest::Approx(standardDeviation));
+}
+
 TEST_CASE("Same number repeatedly") {
     Statistic stat;
     const double onlyNumber = 10;
@@ -53,24 +66,30 @@ TEST_CASE("Same number repeatedly") {
 }
 
 TEST_CASE("Statistics of positive numbers") {
-    const std::vector<double> numbers{
+    const std::vector<double> numbers {
         50.12, 12221.32, 12221.3443, 40001.211,
         9421.11119200, 9994410002.321, 9.0, 10,
         491, 80, 32, 49.91, 48, 389.41, 12.345
     };
-
-    double mean = calculateMean(numbers);
-    double standardDeviation = calculateStandardDeviation(numbers);
-
-    Statistic stat;
-    for(double i : numbers) {
-        stat.add(i);
-    }
-    
-    CHECK(stat.getAverage() == mean);
-    CHECK(stat.getSTD() == standardDeviation);
+    verifyStatisticsMatch(numbers);
 }
 
 TEST_CASE("Statistics of Taylor MacDonald's discussion numbers") {
+    const std::vector<double> numbers {
+        72, 72, 79, 79, 81, 81, 79, 77, 77,
+        76, 76, 74, 74, 72, 79, 79, 77, 77,
+        76, 76, 74, 79, 79, 77, 77, 76, 76,
+        74, 72, 72, 79, 79, 81, 81, 79, 77,
+        77, 76, 76, 74, 74, 24
+    };
+    verifyStatisticsMatch(numbers);
+}
 
+TEST_CASE("Statistics of Michael Schumacher's discussion numbers") {
+    const std::vector<double> numbers {
+        12, 21, 1, 33, 1, 111221111, 12 , 20000,
+        112, 21110, 111111, 10, 21101110, 12, 
+        100000, 11100000,
+    };
+    verifyStatisticsMatch(numbers);
 }
