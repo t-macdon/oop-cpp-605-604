0bbbc7130402b8edcec726be4185ec5adfd28365
Tyler MacDonald <tcmacd18@gmail.com>
2021-09-24 03:24:55 +0000

    main.cpp for PokerRanking WIP

--- PokerRanking/src/main.cpp
+++ PokerRanking/src/main.cpp
@@ -6,9 +6,30 @@
  * @date 2021-09-23
  */
 
+#include <fstream>
+#include <iostream>
 #include <json/json.h>
 
-int main()
-{
+using namespace std;
 
+int main(int argc, char *argv[])
+{
+    // get the json filepath from args
+    if (argc != 2)
+    {
+        cout << "Expected 1 argument to json filepath" << endl;
+        return 1;
+    }
+
+    // save json filepath in string
+    string jsonFilepath(argv[1]);
+
+    // read in the json file
+    Json::Value root;
+    std::ifstream config_doc(jsonFilepath);
+    config_doc >> root;
+
+    cout << root << endl;
+
+    return 0;
 }
