e1ecc2c0acb713711617b74efe3c1a42d3a67732
Tyler MacDonald <tcmacd18@gmail.com>
2021-09-26 16:10:45 +0000

    category and score in hand header

--- PokerRanking/src/pokerRanking/include/Hand.h
+++ PokerRanking/src/pokerRanking/include/Hand.h
@@ -9,13 +9,33 @@
 class Hand
 {
 public:
+    enum class Category
+    {
+        STRAIGHT_FLUSH,
+        FULL_HOUSE,
+        FLUSH,
+        STRAIGHT,
+        FOUR_OF_A_KIND,
+        THREE_OF_A_KIND,
+        TWO_OF_A_KIND,
+        NONE,
+        INCOMPLETE
+    };
     Hand();
     static const int HAND_SIZE = 5;
     std::string toString();
     bool addCard(Card card);
     Card getCard(int i);
+    Category getCategory();
 private:
+    void determineCategory();
     std::vector<Card> handVector;
+    Category category = Category::INCOMPLETE;
+
+    /**
+     * @brief Score is only used when the card category matches that of another hand
+     */
+    int score = 0;
 };
 
 #endif
