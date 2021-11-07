8aba3973e62528384e65ea4a7e66ab3a3d2dbcd0
Tyler MacDonald <tcmacd18@gmail.com>
2021-11-07 21:49:46 +0000

    Rest of the comments

--- ExpressionTree/src/ExpressionTree/include/Tree.h
+++ ExpressionTree/src/ExpressionTree/include/Tree.h
@@ -18,20 +18,96 @@ class Node;
 
 class Tree {
     public:
+
+        /**
+         * @brief Construct a new Tree object
+         * 
+         * @param node 
+         */
         Tree(const Node& node);
+
+        /**
+         * @brief Destroy the Tree object
+         * 
+         */
         ~Tree();
+
+        /**
+         * @brief Get the Symbol object
+         * 
+         * @param symbol 
+         * @return double 
+         */
         double getSymbol(const std::string& symbol) const;
+
+        /**
+         * @brief Set the Symbol object
+         * 
+         * @param symbol 
+         * @param value 
+         */
         void setSymbol(const std::string& symbol, double value);
+
+        /**
+         * @brief Determine of the symbol exists in the symbol table
+         * 
+         * @param symbol 
+         * @return true If symbol is in the table
+         * @return false If symbol is not in the table
+         */
         bool hasSymbol(const std::string& symbol) const;
+
+        /**
+         * @brief Set the Symbol Table object (std::map)
+         * 
+         * @param symbolTable 
+         */
         void setSymbolTable(const std::map<std::string, double>& symbolTable);
+
+        /**
+         * @brief Computes the value of this Tree by traversing the Nodes
+         * 
+         * @return double The computed value
+         */
         double evaluate() const;
+
+        /**
+         * @brief Computes the derivative by traversing the Tree's Nodes
+         * 
+         * @param d Name of the Variable being derived
+         * @return Tree 
+         */
         Tree derivate(const std::string& d) const;
+
+        /**
+         * @brief String representation of this Tree
+         * 
+         * @return std::string 
+         */
         std::string toString() const;
     private:
+
+        /**
+         * @brief Mapping of Variable symbols to their appointed values
+         * 
+         */
         std::map<std::string, double> symbolTable;
+
+        /**
+         * @brief Pointer to this Tree's root node
+         * 
+         */
         Node* node;
 };
 
+
+/**
+ * @brief Output stream operator overload
+ * 
+ * @param os 
+ * @param node 
+ * @return std::ostream& 
+ */
 std::ostream& operator<<(std::ostream& os, const Tree& tree);
 
 #endif // _TREE_H_
--- ExpressionTree/src/ExpressionTree/include/Variable.h
+++ ExpressionTree/src/ExpressionTree/include/Variable.h
@@ -15,14 +15,61 @@
 
 class Variable : public Leaf {
     public:
+
+        /**
+         * @brief Construct a new Variable object
+         * 
+         * @param varName Name of the Variable
+         */
         Variable(std::string varName);
+
+        /**
+         * @brief Destroy the Variable object
+         * 
+         */
         ~Variable();
+
+        /**
+         * @brief Creates a String representation of the object
+         * 
+         * @return std::string 
+         */
         virtual std::string toString() const override final;
+
+        /**
+         * @brief Clones this Variable
+         * 
+         * @return Node* Pointer to the clone
+         */
         virtual Node* clone() const override final;
+
+        /**
+         * @brief Computes this Variable's derivative (either 0 or 1)
+         * 
+         * @return Node*
+         */
         virtual Node* derivate(const std::string&) const override final;
+
+        /**
+         * @brief Get the Variable Name object
+         * 
+         * @return std::string 
+         */
         std::string getVariableName() const { return this->varName; }
+
+        /**
+         * @brief Computes the value of this Variable
+         * 
+         * @return double This variable's value
+         */
         virtual double evaluate() const override final;
+
     private:
+
+        /**
+         * @brief Name of the Variable
+         * 
+         */
         const std::string varName;
 };
 
