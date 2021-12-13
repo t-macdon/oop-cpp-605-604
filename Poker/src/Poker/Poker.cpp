b34581cc81c8b6fea9aa5f30ea81b15cc71f9537
Michael Schumacher <schuma.mike@gmail.com>
2021-12-13 01:07:13 +0000

    Default to prompting for input

--- Poker/src/Poker/Poker.cpp
+++ Poker/src/Poker/Poker.cpp
@@ -15,11 +15,12 @@ int main() {
          << "$" << ANTE_AMOUNT
          << endl << endl;
 
-    // dealer.promptForPlayers();
     // Statically set up some players for test
-    dealer.loadPlayer("Michael", 25.00, false);
-    dealer.loadPlayer("CPU1", 25.00, true);
-    dealer.loadPlayer("CPU2", 25.00, true);
+    // dealer.loadPlayer("Michael", 25.00, false);
+    // dealer.loadPlayer("CPU1", 25.00, true);
+    // dealer.loadPlayer("CPU2", 25.00, true);
+
+    dealer.promptForPlayers();
 
     while(dealer.readyToPlay()) {
         dealer.playARound();
